007ab0d698c3d165ca5e203dc7ca115d2da0c668
esther-v <esvalas@gmail.com>
2022-03-11 16:49:45 +0100

    userReducer ok

--- src/helper/RequireAuth.js
+++ src/helper/RequireAuth.js
@@ -1,12 +1,14 @@
 import React, {useEffect} from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-//import { checkToken } from '../api/user';
+import { checkToken } from '../api/user';
+import {loginUserReducer} from '../lib/redux/user/userReducer'
 
 export default function RequireAuth({children, withAuth}) {
   
-    const user = useSelector(state=>state.user)
+    const user = useSelector(state => state.user)
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     useEffect(()=> {
         if(!user.isLogged && withAuth){
@@ -14,9 +16,17 @@ export default function RequireAuth({children, withAuth}) {
             if(token === null){
                 return navigate("/login")
             } 
-            // else {
-            //     checkToken(token)
-            // }
+            else {
+                checkToken(token)
+                .then((res) => {
+                    console.log(res)
+                    if(res.status !== 200){
+                        return navigate("/login")
+                    } else {
+                        dispatch(loginUserReducer(res.data.user))
+                    }
+                })
+            }
             
         }
     })
