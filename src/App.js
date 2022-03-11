96f518fa0b7b8353e075dc466c56fd144f6b5707
esther-v <esvalas@gmail.com>
2022-03-11 16:54:49 +0100

    render components in app ok

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './components/home.jsx'
 import Container from './components/layout/container';
+import Register from './components/user/register';
+import Login from './components/user/login';
 import RequireAuth from './helper/RequireAuth';
 
 function App() {
@@ -10,8 +12,22 @@ function App() {
       <Container>
         <Routes>
           <Route path="/" element={
-            <RequireAuth>
+            <RequireAuth withAuth={true}>
               <Home/>
+            </RequireAuth>
+          }/> 
+        </Routes>
+        <Routes>
+          <Route path="/register" element={
+            <RequireAuth withAuth={false}>
+              <Register/>
+            </RequireAuth>
+          }/> 
+        </Routes>
+        <Routes>
+          <Route path="/login" element={
+            <RequireAuth withAuth={false}>
+              <Login/>
             </RequireAuth>
           }/> 
         </Routes>
