ffe6eb871deec7943a7bc6ba5b11c051718b7dbd
esther-v <esvalas@gmail.com>
2022-04-11 21:44:53 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import Home from './components/home';
 import Register from './components/user/register';
 import Login from './components/user/login';
 import RequireAuth from './helper/RequireAuth';
+import Topics from './components/topic/topics';
+import AddTopic from './components/topic/addTopic';
+import Topic from './components/topic/topic';
+import AddMessage from './components/message/addMessage';
+import Profil from './components/profil/profil';
+import Admin from './components/admin/admin';
+import UpdateMessage from './components/message/updateMessage';
 
 function App() {
   return (
@@ -12,23 +19,55 @@ function App() {
       <Container>
         <Routes>
           <Route path="/" element={
-              <RequireAuth withAuth={false}>
+              <RequireAuth withAuth={true}>
                 <Home/>
-              </RequireAuth>
-            }/>
-        </Routes>
-        <Routes>
+              </RequireAuth>}/>
+     
           <Route path="/register" element={
             <RequireAuth withAuth={false}>
               <Register/> 
-            </RequireAuth>
-          }/>
-        </Routes>
-        <Routes>
+            </RequireAuth>}/>
+        
           <Route path="/login" element={
             <RequireAuth withAuth={false}>
               <Login/>
             </RequireAuth>}/>
+
+          <Route path="/topics" element={
+            <RequireAuth withAuth={true}>
+              <Topics/>
+            </RequireAuth>}/>
+
+          <Route path="/addTopic" element={
+            <RequireAuth withAuth={true}>
+              <AddTopic/>
+            </RequireAuth>}/>
+
+          <Route path="/topic/:id" element={
+            <RequireAuth withAuth={true}>
+              <Topic/>
+            </RequireAuth>}/>
+
+          <Route path="/addMessage/:topic_id" element={
+            <RequireAuth withAuth={true}>
+              <AddMessage/>
+            </RequireAuth>}/>
+
+          <Route path="admin/updateMessage/:message_id" element={
+            <RequireAuth withAuth={true}>
+              <UpdateMessage/>
+            </RequireAuth>}/>
+
+          <Route path="/profil" element={
+            <RequireAuth withAuth={true}>
+              <Profil/>
+            </RequireAuth>}/>
+
+          <Route path="/admin" element={
+            <RequireAuth withAuth={true}>
+              <Admin/>
+            </RequireAuth>}/>
+
         </Routes>
       </Container>
     </div>
